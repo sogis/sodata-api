f6c17eb0801c768242d3b1553d761f09ef49935b
Stefan Ziegler <edi.gonzales@gmail.com>
2023-02-21 16:37:40 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/sodata/SodataListingsApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.sodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodataListingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SodataListingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/sodata/SodataListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.sodata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodataListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
